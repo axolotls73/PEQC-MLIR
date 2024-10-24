19304b8ac8b8116d78aae2472992e7a48658529b
Emily Tucker <etuck73@colostate.edu>
2024-10-24 16:38:00 +0000

    support herd conversion

--- lib/convert/ConvertToScfParallel.cpp
+++ lib/convert/ConvertToScfParallel.cpp
@@ -5,15 +5,17 @@
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
 //===----------------------------------------------------------------------===//
-// #include "mlir/Dialect/Func/IR/FuncOps.h"
-// #include "mlir/Dialect/MemRef/IR/MemRef.h"
-#include "llvm/ADT/STLExtras.h"
-#include "mlir/InitAllDialects.h"
+
+#include "mlir/Transforms/DialectConversion.h"
 #include "mlir/IR/PatternMatch.h"
-#include "mlir/Rewrite/FrozenRewritePatternSet.h"
 #include "mlir/Support/LogicalResult.h"
-#include "mlir/Transforms/GreedyPatternRewriteDriver.h"
+
+#include "mlir/Dialect/Func/IR/FuncOps.h"
+#include "mlir/Dialect/MemRef/IR/MemRef.h"
+#include "mlir/Dialect/SCF/IR/SCF.h"
+#include "mlir/Dialect/Async/IR/Async.h"
 #include "air/Dialect/AIR/AIRDialect.h"
+#include "llvm/ADT/STLExtras.h"
 
 #include "VerifPasses.h"
 #include "VerifDialect.h"
@@ -55,8 +57,46 @@ public:
 
   LogicalResult matchAndRewrite(xilinx::air::HerdOp op, OpAdaptor adaptor,
                                 ConversionPatternRewriter &rewriter) const final {
-    //
-    return failure();
+    auto loc = op.getLoc();
+
+    Value cst0 = rewriter.create<arith::ConstantIndexOp>(loc, 0);
+    Value cst1 = rewriter.create<arith::ConstantIndexOp>(loc, 1);
+    auto parop = rewriter.create<scf::ParallelOp>(loc,
+        SmallVector<Value>{cst0, cst0}, op.getSizes(), SmallVector<Value>{cst1, cst1},
+        [&](OpBuilder& b, Location loc, ValueRange ivs) {
+
+          // herd arguments are: iterator, upper bounds, args
+          // want to map all to their operands in cloned body
+          SmallVector<Value> argvals;
+          for (auto i : ivs) {
+            argvals.push_back(i);
+          }
+          for (auto s : op.getSizes()) {
+            argvals.push_back(s);
+          }
+          for (auto o : op.getHerdOperands()) {
+            argvals.push_back(o);
+          }
+
+          IRMapping mapper;
+          for (auto [argval, herdval] : llvm::zip_equal(
+              argvals, op.getBodyRegion().getArguments())) {
+            mapper.map(herdval, argval);
+          }
+
+          for (auto &o : op.getOps()) {
+            if (isa<xilinx::air::HerdTerminatorOp>(o)) continue;
+
+            b.clone(o, mapper);
+          }
+          b.create<scf::ReduceOp>(loc);
+
+        });
+
+    // op.getOperation()->getParentOfType<ModuleOp>().emitWarning();
+
+    rewriter.eraseOp(op);
+    return success();
   }
 };
 
@@ -66,7 +106,49 @@ public:
   using impl::VerifAirToScfParBase<VerifAirToScfPar>::VerifAirToScfParBase;
 
   void runOnOperation() final {
+    auto module = getOperation();
+    auto context = module.getContext();
 
+    TypeConverter converter;
+    converter.addConversion([&](Type type) -> std::optional<Type> {
+      // convert air::AsyncTokenType to async::TokenType
+      if (auto t = type.dyn_cast<xilinx::air::AsyncTokenType>())
+        return async::TokenType::get(context);
+      if (auto t = type.dyn_cast<MemRefType>())
+        if (t.getMemorySpaceAsInt() != 0)
+          return MemRefType::get(t.getShape(), t.getElementType(),
+                                 t.getLayout(), 0);
+      return type;
+    });
+    auto addUnrealizedCast = [](OpBuilder &builder, Type type,
+                                ValueRange inputs, Location loc) {
+      auto cast = builder.create<UnrealizedConversionCastOp>(loc, type, inputs);
+      return std::optional<Value>(cast.getResult(0));
+    };
+    converter.addSourceMaterialization(addUnrealizedCast);
+    converter.addTargetMaterialization(addUnrealizedCast);
+
+    mlir::RewritePatternSet patterns(context);
+    patterns.add<
+        // VerifLaunchPattern,
+        // VerifSegmentPattern,
+        VerifHerdPattern
+      >(converter, context);
+
+    ConversionTarget target(*context);
+
+    target.addLegalDialect<
+        func::FuncDialect,
+        arith::ArithDialect,
+        scf::SCFDialect,
+        memref::MemRefDialect,
+        async::AsyncDialect
+        // ,
+        // mlir::BuiltinDialect
+      >();
+
+    auto res = applyPartialConversion(module, target, std::move(patterns));
+    if (res.failed()) return signalPassFailure();
   }
 };
 
