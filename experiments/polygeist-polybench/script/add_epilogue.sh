796746a38f59c1ca60e869bfdfd576cb2f804552
Emily Tucker <birdsarelegit@gmail.com>
2026-04-08 03:52:16 +0000

    move to interp_macros in repo

--- experiments/polygeist-polybench/script/add_epilogue.sh
+++ experiments/polygeist-polybench/script/add_epilogue.sh
@@ -5,7 +5,7 @@ epfile=$2
 outfile=$3
 projectroot=$4
 
-VTDIR=$projectroot/experiments/polygeist-polybench/script
+VTDIR=$projectroot/tools/verif-translate/include
 VTDIR=`realpath $VTDIR`
 VTDIR=${VTDIR//\//\\/}
 
